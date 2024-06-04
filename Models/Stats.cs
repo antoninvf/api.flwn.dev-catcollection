43406f8d391ee7d7e53f26a0963f30f4bb499ca3
Antonín <flowythe@gmail.com>
2024-06-04 11:41:38 +0200

    model for showing stats

---
+++ Models/Stats.cs
@@ -0,0 +1,10 @@
+﻿namespace CatCollectionAPI;
+
+public class Stats
+{
+    public int FileCount { get; set; } = 0;
+    public int ImageCount { get; set; } = 0;
+    public int VideoCount { get; set; } = 0;
+    public int CategoryCount { get; set; } = 0;
+    public List<string> Categories { get; set; } = new List<string>();
+}
