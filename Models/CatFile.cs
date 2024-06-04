1fd487375c09be69329b712e6fed5fe6b1b86826
Antonín <flowythe@gmail.com>
2024-06-04 11:41:41 +0200

    moved

---
+++ Models/CatFile.cs
@@ -0,0 +1,8 @@
+﻿namespace CatCollectionAPI;
+
+public class CatFile
+{
+    public string FileName { get; set; } = "";
+    public string Link { get; set; } = "";
+    public string Category { get; set; } = "";
+}
